a9379391258f1772f6b6eb9f2da8fe6f9659c575
Julian Langschaedel <meta.rb@gmail.com>
2012-07-07 02:25:41 +0200

    add MultiWii 2.1-rc2
      MD5: e4b31d9fc01496c49b5d580500fade97  MultiWii_release_candidate_2_1_r964.zip
    
      svn: http://multiwii.googlecode.com/svn/trunk@964 - nils.tholen@gmail.com - Thu Jul 5 18:34:41 2012 +0000
    
        multiwii release candidate 2.1 - r964
    
        http://www.multiwii.com/forum/viewtopic.php?f=8&t=2007 # Let's try to release 2.1: second try based on r964

--- MultiWii/def.h
+++ MultiWii/def.h
@@ -1446,9 +1446,17 @@
   #endif
 #endif
 
+#if !defined(ALT_HOLD_THROTTLE_NEUTRAL_ZONE)
+  #define ALT_HOLD_THROTTLE_NEUTRAL_ZONE 20
+#endif 
+
 /**************************************************************************************/
 /***************               Error Checking Section              ********************/
 /**************************************************************************************/
+
+#ifndef NUMBER_MOTOR
+        #error "NUMBER_MOTOR is not set, most likely you have not defined any type of multicopter"
+#endif
 
 #if (defined(LCD_CONF) || defined(LCD_TELEMETRY)) && !(defined(LCD_SERIAL3W) || defined(LCD_TEXTSTAR) || defined(LCD_VT100) || defined(LCD_ETPP) || defined(LCD_LCD03) || defined(OLED_I2C_128x64) )
   #error "LCD_CONF or LCD_TELEMETRY defined, and choice of LCD not defined.  Uncomment one of LCD_SERIAL3W or LCD_TEXTSTAR or LCD_VT100 or LCD_ETPP or LCD_LCD03 or OLED_I2C_128x64"
